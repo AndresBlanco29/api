36b105f90d8cf5d735eb70a95e76f8c5521f0478
SoyKmilo <yocrac@outlook.com>
2025-05-09 15:49:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Cambiar a dominio real si es necesario
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
